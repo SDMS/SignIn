b8a0f68f5784ff732eac34334d6863a519ce256b
Eric Sims <ericsimsm@gmail.com>
2014-11-03 20:40:36 -0500

    socket working, added package.json. added important dork function.

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
 	res.sendfile('./classmap.html');
 });
 
-var server = app.listen(3000, function() {
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('dork', function(socket) {
+		console.log('dokrekrdorkdorkd');
+	});
+});
+
+
+var server = http.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 	
