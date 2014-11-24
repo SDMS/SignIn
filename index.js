3f5fd0ec5eac78021ba1202e75f065f110a03762
Eric Sims <ericsimsm@gmail.com>
2014-11-23 21:45:04 -0500

    client side of sign in is in good shape. still need to implement server side sql stuff and everything with signing out

--- index.js
+++ index.js
@@ -14,14 +14,28 @@ app.get('/signin.js', function(req, res) {
 
 io.on('connection', function(socket) {
 	console.log('a user connected');
+	
 	socket.on('sign in', function(data) {
 		console.log('submitted ' + JSON.stringify(data));
-		});
+	});
+	
+	socket.on('check student', function(message){
+		if(false) { // student not in database 
+			socket.emit('check fail', "Student ID not in database.");
+			console.log('check fail: 1');
+		} else if(false){ // student already signed in
+			socket.emit('check fail', "You are already signed in.");
+			console.log('check fail: 2');
+		} else {
+			socket.emit('check success');
+			console.log('check success');
+		}
+	});
 });
 
 var server = http.listen(3000, function() {
 	var host = server.address().address;
 	var port = server.address().port;
-	
+
 	console.log('App listening at http://%s%s', host, port);
 });
--- signin.js
+++ signin.js
@@ -1,9 +1,39 @@
 var socket = io();
 
-var selectedComputer;
+var selectedComputer = -1;
 
-	function signin() {
-		var sid = document.getElementById('sid');
-		socket.emit('sign in', {"id": sid.value});
-		console.log('sid submitted: ' + sid.value);
+function signin() {
+	var sid = document.getElementById('sid').value;
+	var message = {"id": sid, "computer": selectedComputer};
+	if(selectedComputer == -1) {
+		alert('Please select a computer');
+		return;
 	}
+	socket.emit('check student', sid); 	// check if student exists, check if student is already signed in
+	socket.on('check success', function(){
+		socket.emit('sign in', message);
+		 alert('student id ' + sid + ' at computer ' + selectedComputer);
+		// display student's information
+		document.getElementById(selectedComputer).className = "computer taken";
+		// clear text box
+		selectedComputer = -1;
+	});
+	socket.on('check fail', function(message){
+		alert('error: ' + message);
+		// clear text box
+	});
+}
+
+function chooseComputer(id) {
+
+	// check if new computer is already occupied
+	if(document.getElementById(id).className != "computer taken") {
+		// deselect old computer
+		if(selectedComputer != -1) { // if there was already a previously selected computer
+			document.getElementById(selectedComputer).className = "computer";
+		}
+		// select new computer
+		selectedComputer = id;
+		document.getElementById(id).className = "computer selected";
+	}
+}
