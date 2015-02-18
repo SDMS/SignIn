26b83710dc97b7aab53b0eab982a49df5e08c313
Giselle Koo <gisellegk@gmail.com>
2015-02-18 16:27:07 -0500

    started working on a class-map generator

---
+++ generator.js
@@ -0,0 +1,22 @@
+// simplest classroom
+var basic = {
+	school: "Silas Deane Middle School",
+	roomName: "Media Center",
+	bySeat: false,
+	signOut: false
+};
+
+var classroom = {
+	school: "Silas Deane Middle School",
+	roomName: "Mac Lab",
+	bySeat: true,
+	signOut: true,
+	numSeats: 31,
+	seatOrientation: {
+		row1: [1,2,3,4,5,6,7,8],
+		row2: [9,10,11,12,13,14,15,16],
+		row3: [17,18,19,20,21,22,23,24],
+		row4: [25,26,27,28,29,30,31]
+	}
+	
+};
