de2371a3aa1572d06148c34299cb01df077b7943
Giselle <gisellegk@gmail.com>
2015-03-13 15:32:12 -0400

    began draft of potential way to write html of the room based on params

--- generator.js
+++ generator.js
@@ -18,4 +18,22 @@
  *
 */
 
+var layout_1 = {
+	name: 'layout_1',
+	css: 'css/layout_1.css',
+	js: 'js/layout_1.js',
+	signInTabContent: 'Student ID: <input id="sid" type="text" maxlength="8" autofocus /> <div class="button" onclick="signin()">Go</div>',
+	signInTabContentChoices: '<div id="signin-instructions" class="accordion active"> Click on a computer to sign in! </div>' +
+				 '<div id="signin-options" class="accordion">' +
+				 '<div id="choice-container"></div>' + //put choices here. also needs to be implemented in CSS & tested
+				 '<div class="button" onclick="chooseComputer(selectedComputer)">Cancel</div><div class="button" onclick="signin()">Go</div>' + //can we dry this up
+				 '</div>',
+	signOutTabContent: 'Click on your name to sign out. <div class="button" onclick="signin()">Go</div>',
+	signOutTabContentChoices: '<div id="signout-instructions" class="accordion active"> Click on your name to sign out! </div>' +
+				  '<div id="signout-options" class="accordion">' +
+				  '<div id="destination-container"></div> ' + // put choices in here
+				  '<div class="button" onclick="chooseComputer(selectedComputer)">Cancel</div><div class="button" onclick="signout()">Go</div>' +
+				  '</div>'
+}
+
 var testRooms = require('./rooms.js');
